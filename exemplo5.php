3e7f0878b2317d04dfe0d026150cf23f898ee154
Igor Brito <igor_pbrito@hotmail.com>
2023-08-23 12:02:53 -0300

    Commit inicial

---
+++ exemplo5.php
@@ -0,0 +1,12 @@
+<?php
+
+# calculo de area de um retamgulo
+
+$base = 3;
+$altura = 8;
+
+$area = $base * $altura;
+
+echo $area;
+
+?>
---
+++ igorbrito.php
@@ -0,0 +1,11 @@
+<?php
+
+$nome = "Igor";
+$sobrenome = "Brito";
+$idade = "32";
+$profissao = "estudante";
+
+echo "Olá $profissao $nome $sobrenome.";
+echo "Você tem atualmente $idade anos?";
+
+?>
